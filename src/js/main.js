65540a443a9eb5f81360b155ae52e3df186a6923
Oleksandr Pereiaslov <Oleksandr@MacBook-Pro-Oleksandr.local>
2021-09-01 19:31:58 +0300

    message

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,9 @@ import documentReady from "./helpers/documentReady";
 documentReady(() => {
 	lazyImages();
 });
+ScrollReveal().reveal(".headline1", { delay: 400, distance: "50px" });
+ScrollReveal().reveal(".headline2", { delay: 500, distance: "50px" });
+ScrollReveal().reveal(".headline3", { delay: 600, distance: "50px" });
+ScrollReveal().reveal(".headline4", { delay: 1100, distance: "50px" });
+ScrollReveal().reveal(".headline5", { delay: 1300, distance: "50px" });
+ScrollReveal().reveal(".headline6", { delay: 1500, distance: "50px" });
