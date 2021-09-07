645607c50de10e0beaa54e8a3e46c9971c63ae68
Oleksandr Pereiaslov <Oleksandr@MacBook-Pro-Oleksandr.local>
2021-09-07 15:12:23 +0300

    message

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,15 @@ ScrollReveal().reveal(".headline3", { delay: 600, distance: "50px" });
 ScrollReveal().reveal(".headline4", { delay: 1100, distance: "50px" });
 ScrollReveal().reveal(".headline5", { delay: 1300, distance: "50px" });
 ScrollReveal().reveal(".headline6", { delay: 1500, distance: "50px" });
+
+const trigger = document.querySelector(".burger"); // or whatever triggers the toggle
+const element = document.querySelector(".nav-menu ");
+const body = document.querySelector(".page-home");
+
+console.log(trigger);
+
+trigger.addEventListener("click", function (e) {
+	e.preventDefault();
+	element.classList.toggle("active"); // or whatever your active class is
+	body.classList.toggle("sidebar-is-open"); // or whatever your active class is
+});
