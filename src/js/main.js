c66401ff669bd36629c9e60cf7557f84eaac19ac
Oleksandr Pereiaslov <Oleksandr@MacBook-Pro-Oleksandr.local>
2021-09-14 03:27:01 +0300

    message

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import "focus-visible";
 import lazyImages from "./modules/lazyImages";
 import documentReady from "./helpers/documentReady";
+import "./menucross";
 
 documentReady(() => {
 	lazyImages();
@@ -8,18 +9,18 @@ documentReady(() => {
 ScrollReveal().reveal(".headline1", { delay: 400, distance: "50px" });
 ScrollReveal().reveal(".headline2", { delay: 500, distance: "50px" });
 ScrollReveal().reveal(".headline3", { delay: 600, distance: "50px" });
-ScrollReveal().reveal(".headline4", { delay: 1100, distance: "50px" });
-ScrollReveal().reveal(".headline5", { delay: 1300, distance: "50px" });
-ScrollReveal().reveal(".headline6", { delay: 1500, distance: "50px" });
+ScrollReveal().reveal(".headline4", { delay: 700, distance: "50px" });
+ScrollReveal().reveal(".headline5", { delay: 800, distance: "50px" });
+ScrollReveal().reveal(".headline6", { delay: 900, distance: "50px" });
 
 const trigger = document.querySelector(".burger"); // or whatever triggers the toggle
 const element = document.querySelector(".nav-menu ");
-const body = document.querySelector(".page-home");
+const body = document.querySelector("body");
 
 console.log(trigger);
 
 trigger.addEventListener("click", function (e) {
 	e.preventDefault();
 	element.classList.toggle("active"); // or whatever your active class is
-	body.classList.toggle("sidebar-is-open"); // or whatever your active class is
+	body.classList.toggle("active"); // or whatever your active class is
 });
---
+++ src/js/menucross.js
@@ -0,0 +1,11 @@
+const trigger = document.querySelector(".burger"); // or whatever triggers the toggle
+const element = document.querySelector(".menu ");
+const body = document.querySelector(".page-home");
+
+console.log(trigger);
+
+trigger.addEventListener("click", function (e) {
+	e.preventDefault();
+	element.classList.toggle("active"); // or whatever your active class is
+	body.classList.toggle("sidebar-is-open"); // or whatever your active class is
+});
